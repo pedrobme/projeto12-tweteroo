b6c707e98455a99bf04348d04c40b2372c00ed82
pedrohsa <pedroholanda94@gmail.com>
2022-11-05 14:37:28 -0300

    POST sign-up

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express()
+
+app.use(express.json())
+
+const users = []
+
+app.post('/sign-up', (req,res) => {
+
+    users.push(req.body)
+
+    console.log(users)
+    res.send('OK')
+})
 
 app.listen(5000, ()=>console.log('Server running at port 5000'))
