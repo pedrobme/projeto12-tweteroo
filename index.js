dd58f04c6912cff223698dc6bcb900c4474516ef
pedrohsa <pedroholanda94@gmail.com>
2022-11-05 14:39:34 -0300

    POST tweets

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ app.use(express.json())
 
 const users = []
 
+const tweets = []
+
 app.post('/sign-up', (req,res) => {
 
     users.push(req.body)
 
     console.log(users)
+    res.send('OK')
+})
+
+app.post('/tweets', (req,res) => {
+
+    tweets.push(req.body)
+
+    console.log(tweets)
     res.send('OK')
 })
 
